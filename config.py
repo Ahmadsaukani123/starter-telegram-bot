96a6f3d0d7330d2e812c96ea64c8550a7673ff08
Ahmadsaukani123 <100931857+Ahmadsaukani123@users.noreply.github.com>
2023-03-25 03:09:12 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "kang_culiknew") # jangan dirubah agar tidak error
+GROUP = os.environ.get("GROUP", "") # 
+CHANNEL = os.environ.get("CHANNEL", "vvslh_pro") # 
